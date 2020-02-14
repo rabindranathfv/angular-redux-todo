40f57b6037983e79d263698684e3d28230149c3d
Rabindranath <Ferreira>
2020-02-14 10:37:05 -0300

    all reducer operations are ready

--- src/app/store.ts
+++ src/app/store.ts
@@ -13,17 +13,34 @@ export const INITIAL_STATE: IAppState = {
 
 export function rootReducer(state, action) {
     switch (action.type) {
-        case ADD_TODO:
+        case ADD_TODO: 
             action.todo.id = state.todo.length + 1;
             return Object.assign({}, state, {
-                todos: state.todos.concat(Object.assign({}, action.todo))
+                todos: state.todos.concat(Object.assign({}, action.todo)),
+                lastUpdate: new Date()
             });
         case TOGGLE_TODO:
-            break;
+            let todo = state.todos.find(t => t.id === action.id);
+            let index = state.todos.indexOf(todo);
+            todo.isCompleted = !todo.isCompleted;
+            console.log('TODO INTO TOGGLE TODO', todo);
+            return Object.assign({}, state, {
+                todos: [
+                    ...state.todos,
+                    todo
+                ],
+                lastUpdated: new Date()
+            })
         case REMOVE_TODO:
-            break;
+            return Object.assign({}, state, {
+                todos: state.todos.filter(t => t.id !== action.id),
+                lastUpdated: new Date()
+            })
         case REMOVE_ALL_TODOS:
-            break;
+            return Object.assign({}, state, {
+                todos: [],
+                lastUpdated: new Date()
+            });
         default:
             return state;
     }
